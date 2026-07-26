4077141a18162c73b46137ae92569b4191213a43
Minh Nhat Le <nhatle.stu@gmail.com>
2026-07-26 23:17:07 +0700

    feat: invoke agent with question and check conversation.

--- src/agent.py
+++ src/agent.py
@@ -1,10 +1,13 @@
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
+from langchain.messages import HumanMessage
 
 load_dotenv()
 
 system_prompt = "You are a knowledgeable individual, answer user questions at their requests."
+
+question = HumanMessage(content="What is the capital of Canada?")
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
@@ -16,3 +19,9 @@ root_agent = create_agent(
     model=model,
     system_prompt=system_prompt
 )
+
+response = root_agent.invoke({"messages": [question]})
+
+# Print the conversation
+for m in response["messages"]:
+    m.pretty_print()
