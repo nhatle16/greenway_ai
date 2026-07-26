75059946a8aca4155ea1cc75397d2eb7fdbfb3ed
Minh Nhat Le <nhatle.stu@gmail.com>
2026-07-26 23:22:08 +0700

    feat: define a web search tool using Tavily for the agent.

--- src/agent.py
+++ src/agent.py
@@ -2,8 +2,19 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.agents import create_agent
 from langchain.messages import HumanMessage
+from langchain.tools import tool
+from tavily import TavilyClient
 
 load_dotenv()
+
+# Create a Tavily client for web search
+tavily_client = TavilyClient()
+
+# Define a web search tool
+@tool
+def web_search(query: str):
+    """Search information from the web."""
+    return tavily_client.search(query=query)
 
 system_prompt = "You are a knowledgeable individual, answer user questions at their requests."
 
