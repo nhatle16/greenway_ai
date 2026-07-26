bec2c991af63f05f9e15aef173c810c1eabd7618
Minh Nhat Le <nhatle.stu@gmail.com>
2026-07-26 23:03:18 +0700

    Initialized a LLM instance using Google GenAI.

--- src/agent.py
+++ src/agent.py
@@ -1,1 +1,12 @@
-print("Hello World")
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5
+)
+
+response = llm.invoke("Hello from Nolan")
+print(response)
