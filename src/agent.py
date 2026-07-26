2a5c7e5b26ee90d4154153671c0f1a25cfdb25a0
Minh Nhat Le <nhatle.stu@gmail.com>
2026-07-26 23:27:40 +0700

    refactor: provide agent with tool for web searching.

--- src/agent.py
+++ src/agent.py
@@ -7,8 +7,10 @@ from tavily import TavilyClient
 
 load_dotenv()
 
+
 # Create a Tavily client for web search
 tavily_client = TavilyClient()
+
 
 # Define a web search tool
 @tool
@@ -16,22 +18,31 @@ def web_search(query: str):
     """Search information from the web."""
     return tavily_client.search(query=query)
 
-system_prompt = "You are a knowledgeable individual, answer user questions at their requests."
+system_prompt = """
 
-question = HumanMessage(content="What is the capital of Canada?")
+You are a knowledgeable individual, answer user questions at their requests.
+
+Using the web search tool, find the latest and correct information from the web to answer user.
+
+"""
+
+question = HumanMessage(content="Who is the current Prime Minister of Canada?")
 
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.5
 )
 
+
 # Define a root agent
 root_agent = create_agent(
     model=model,
+    tools=[web_search],
     system_prompt=system_prompt
 )
 
 response = root_agent.invoke({"messages": [question]})
+
 
 # Print the conversation
 for m in response["messages"]:
