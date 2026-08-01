8ee67d2a33b81fbd434ff473ce85631244e1b439
Minh Nhat Le <nhatle.stu@gmail.com>
2026-08-01 20:25:19 +0700

    fix: load Tavily API key to power up web_search

--- src/tools/search.py
+++ src/tools/search.py
@@ -1,7 +1,11 @@
-from langchain.tools import tool
 from tavily import TavilyClient
+from langchain.tools import tool
+from dotenv import load_dotenv
+
+load_dotenv()
 
 tavily_client = TavilyClient()
+
 
 @tool
 def web_search(query: str):
